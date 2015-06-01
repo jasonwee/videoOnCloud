460877ffb79b217856fb576d9f60d93e89faf2d4
Jason Wee <peichieh@gmail.com>
2015-06-01 15:34:12 +0800

    learn java8 boolean newly added method.

---
+++ src/java/play/learn/java/LearnBoolean.java
@@ -0,0 +1,26 @@
+package play.learn.java;
+
+public class LearnBoolean {
+
+	public static void main(String[] args) {
+		
+		// java8
+		int value = Boolean.hashCode(true);
+		System.out.println(value);
+		
+		// java8
+		value = Boolean.hashCode(true);
+		System.out.println(value);
+		
+		// java8
+		System.out.println(Boolean.logicalAnd(false, true));
+
+		// java8
+		System.out.println(Boolean.logicalOr(false, true));
+		
+		// java8
+		System.out.println(Boolean.logicalXor(true, true));
+
+	}
+
+}
