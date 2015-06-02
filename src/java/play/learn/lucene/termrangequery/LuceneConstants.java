7e1844ad784a9029ba68561bfbe63e50dc37c961
Jason Wee <peichieh@gmail.com>
2015-06-03 01:51:56 +0800

    doc

--- src/java/play/learn/lucene/termrangequery/LuceneConstants.java
+++ src/java/play/learn/lucene/termrangequery/LuceneConstants.java
@@ -1,12 +1,15 @@
 package play.learn.lucene.termrangequery;
 
+/**
+ * setting for this application
+ * 
+ * @author jason
+ *
+ */
 public class LuceneConstants {
 	
 	public static final String CONTENTS = "contents";
 	public static final String FILE_NAME = "filename";
 	public static final String FILE_PATH = "filepath";
 	public static final int MAX_SEARCH = 10;
-	
-	
-
 }
