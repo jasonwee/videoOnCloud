f1b0f5f76d90c28166322bebb8cb638ddda037de
Jason Wee <peichieh@gmail.com>
2015-06-30 23:01:17 +0800

    #9 learn volatile

---
+++ src/java/play/learn/java/LearnVolatile.java
@@ -0,0 +1,44 @@
+package play.learn.java;
+
+public class LearnVolatile {
+	
+    private static volatile int MY_INT = 0;
+
+    public static void main(String[] args) throws InterruptedException
+    {
+        new ChangeListener().start();
+        //System.out.println("Waiting two seconds so the JIT will probably optimize ChangeListener");
+        //Thread.sleep(2000);
+
+        new ChangeMaker().start();
+    }
+
+    static class ChangeListener extends Thread {
+        @Override
+        public void run() {
+            int local_value = MY_INT;
+            while ( local_value < 5){
+                if( local_value!= MY_INT){
+                    System.out.println("Got Change for MY_INT : "+ MY_INT);
+                     local_value= MY_INT;
+                }
+            }
+        }
+    }
+
+    static class ChangeMaker extends Thread {
+        @Override
+        public void run() {
+
+            int local_value = MY_INT;
+            while (MY_INT <5){
+                System.out.println("Incrementing MY_INT to " + (local_value + 1));
+                MY_INT = ++local_value;
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) { e.printStackTrace(); }
+            }
+        }
+    }
+
+}
