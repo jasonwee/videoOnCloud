fe42e5201840dd34d134dca5d00ef14ff58c33d1
Jason Wee <peichieh@gmail.com>
2015-07-22 18:41:24 +0800

    animateSpring, animateAutumn, fakeFallDownAnimation, copyEllipse and added interface HideMethod

--- src/java/play/learn/java/fx/Animator.java
+++ src/java/play/learn/java/fx/Animator.java
@@ -3,15 +3,26 @@ package play.learn.java.fx;
 import java.util.List;
 
 import javafx.animation.Animation;
+import javafx.animation.FadeTransition;
+import javafx.animation.FillTransition;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.ParallelTransition;
 import javafx.animation.PauseTransition;
+import javafx.animation.ScaleTransition;
 import javafx.animation.SequentialTransition;
 import javafx.animation.Timeline;
 import javafx.animation.Transition;
+import javafx.animation.TranslateTransition;
+import javafx.beans.binding.DoubleBinding;
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
 import javafx.scene.transform.Rotate;
 import javafx.util.Duration;
+import static java.lang.Math.*;
+import static play.learn.java.fx.Util.addChildToParent;
 
 public class Animator implements Runnable {
 	
@@ -21,6 +32,7 @@ public class Animator implements Runnable {
 	public static final Duration LEAF_BECOME_YELLOW_DURATION = Duration.seconds(5);
 	public static final Duration WIND_CYCLE_DURATION = Duration.seconds(5);
 	public static final Duration LEAF_APPEARING_DURATION = Duration.seconds(2);
+	public static final Duration FLOWER_APPEARING_DURATION = Duration.seconds(1);
 	private final TreeGenerator treeGenerator;
 	private final GrassGenerator grassGenerator;
 	
@@ -83,13 +95,149 @@ public class Animator implements Runnable {
 		return wind;
 	}
 	
-	private Transition seasonAnimation(final Tree tree, final List<Blade> grass) {
+	private Transition seasonsAnimation(final Tree tree, final List<Blade> grass) {
 		Transition spring = animateSpring(tree.leafage, grass);
 		Transition flowers = animateFlowers(tree.flowers);
 		Transition autumn = animateAutumn(tree.leafage, grass);
 		
 		SequentialTransition sequentialTransition = new SequentialTransition(spring, flowers, autumn);
 		return sequentialTransition;
+	}
+	
+	private Transition animateSpring(List<Leaf> leafage, List<Blade> grass) {
+		ParallelTransition springAnimation = new ParallelTransition();
+		
+		for (final Blade blade : grass) {
+			// grass become green
+			springAnimation.getChildren().add(new FillTransition(GRASS_BECOME_GREEN_DURATION, blade, (Color) blade.getFill(), blade.SPRING_COLOR));
+		}
+		
+		for (Leaf leaf : leafage) {
+			// leafage appear
+			ScaleTransition leafageAppear = new ScaleTransition(LEAF_APPEARING_DURATION, leaf);
+			leafageAppear.setToX(1);
+			leafageAppear.setToY(1);
+			springAnimation.getChildren().add(leafageAppear);
+		}
+		return springAnimation;
+	}
+	
+	private Transition animateFlowers(List<Flower> flowers) {
+		ParallelTransition flowersAppearAndFallDown = new ParallelTransition();
+		
+		for (int i = 0; i < flowers.size(); i++) {
+			final Flower flower = flowers.get(i);
+			for (Ellipse petal : flower.getPetals()) {
+				
+				FadeTransition flowerAppear = new FadeTransition(FLOWER_APPEARING_DURATION, petal);
+				flowerAppear.setToValue(1);
+				flowerAppear.setDelay(FLOWER_APPEARING_DURATION.divide(3).multiply(i + 1));
+				
+				flowersAppearAndFallDown.getChildren().add(new SequentialTransition(new SequentialTransition(
+						//flowes appearing
+						flowerAppear,
+						//fall down
+						fakeFallDownAnimation(petal))));
+			}
+		}
+		return flowersAppearAndFallDown;
+	}
+	
+	private Transition animateAutumn(List<Leaf> leafage, List<Blade> grass) {
+		ParallelTransition autumn = new ParallelTransition();
+		
+		//leafage animation
+		ParallelTransition yellowLeafage = new ParallelTransition();
+		ParallelTransition disappearLeafage = new ParallelTransition();
+		
+		for (final Leaf leaf : leafage) {
+			// become yellow
+			final FillTransition toYellow = new FillTransition(LEAF_BECOME_YELLOW_DURATION, leaf, null, leaf.AUTUMN_COLOR);
+			yellowLeafage.getChildren().add(toYellow);
+			
+			// fall down
+			Animation fakeLaefageDown = fakeFallDownEllipseAnimation(leaf, leaf.AUTUMN_COLOR, node -> {node.setScaleX(0); node.setScaleY(0);});
+			
+			// disappear
+			disappearLeafage.getChildren().add(fakeLaefageDown);
+		}
+		
+		// grass animation
+		ParallelTransition grassBecomeYellowAnimation = new ParallelTransition();
+		for (final Blade blade : grass) {
+			// become yellow
+			final FillTransition toYellow = new FillTransition(GRASS_BECOME_YELLOW_DURATION, blade, (Color)blade.getFill(), blade.AUTUMN_COLOR);
+			toYellow.setDelay(Duration.seconds(1 * random()));
+			grassBecomeYellowAnimation.getChildren().add(toYellow);
+		}
+		autumn.getChildren().addAll(grassBecomeYellowAnimation, new SequentialTransition(yellowLeafage, disappearLeafage));
+		return autumn;
+	}
+	
+	private Animation fakeFallDownAnimation(final Ellipse petalOld) {
+		return fakeFallDownEllipseAnimation(petalOld, null, node ->node.setOpacity(0));
+	}
+	
+	private Animation fakeFallDownEllipseAnimation(final Ellipse sourceEllipse, Color fakeColor, final HideMethod hideMethod) {
+		final Ellipse fake = copyEllipse(sourceEllipse, fakeColor);
+		addChildToParent(treeGenerator.content, fake);
+		
+		PauseTransition replaceFakeWithSource = new PauseTransition(Duration.ONE);
+		replaceFakeWithSource.setDelay(Duration.minutes(0.9 * random() + 0.1));
+		replaceFakeWithSource.setOnFinished(e -> {
+			final Point2D position = treeGenerator.content.sceneToLocal(sourceEllipse.localToScene(0, 0));
+			
+			DoubleBinding sinPath = new DoubleBinding() {
+				
+				{
+					bind(fake.translateYProperty());
+				}
+				
+				@Override
+				protected double computeValue() {
+					return 50 * sin((fake.translateYProperty().doubleValue() - position.getY()) / 20);
+				}
+			};
+			fake.setTranslateY(position.getY());
+			fake.setCenterX(0);
+			fake.setCenterY(0);
+			fake.translateXProperty().bind(sinPath.add(position.getX()));
+			fake.rotateProperty().bind(fake.translateYProperty().multiply(2).add(random() * 180));
+			// replace source with fake
+			fake.setOpacity(1);
+			hideMethod.hide(sourceEllipse);
+		});
+		TranslateTransition fallDown = new TranslateTransition(Duration.seconds(30), fake);
+		fallDown.setToY(random() * 30 + 1);
+		FadeTransition disappear = new FadeTransition(Duration.seconds(2), fake);
+		disappear.setDelay(Duration.seconds(5));
+		disappear.setToValue(0);
+		
+		return new SequentialTransition(replaceFakeWithSource, 
+				// fall down
+				fallDown,
+				// dissappear
+				disappear);
+	}
+	
+	private Ellipse copyEllipse(Ellipse petalOld, Color color) {
+		Ellipse ellipse = new Ellipse();
+		ellipse.setRadiusX(petalOld.getRadiusX());
+		ellipse.setRadiusY(petalOld.getRadiusY());
+		
+		if (color == null) {
+			ellipse.setFill(petalOld.getFill());
+		} else  {
+			ellipse.setFill(color);
+		}
+		ellipse.setRotate(petalOld.getRotate());
+		ellipse.setOpacity(0);
+		return ellipse;
+	}
+	
+	@FunctionalInterface
+	private static interface HideMethod {
+		void hide(Node node);
 	}
 
 }
