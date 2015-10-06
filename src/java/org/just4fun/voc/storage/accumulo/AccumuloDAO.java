151f9313f9ebbd836054f2b52b284e97c11e09f8
Jason Wee <peichieh@gmail.com>
2015-10-06 20:39:23 +0800

    added accumulo

---
+++ src/java/org/just4fun/voc/storage/accumulo/AccumuloDAO.java
@@ -0,0 +1,37 @@
+package org.just4fun.voc.storage.accumulo;
+
+import java.io.IOException;
+
+import org.just4fun.voc.file.VirtualFile;
+import org.just4fun.voc.storage.VirtualFileDAO;
+
+public class AccumuloDAO implements VirtualFileDAO {
+
+	@Override
+	public boolean storeBinary(VirtualFile virtualFile) throws IOException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public VirtualFile readVirtualFile(VirtualFile virtualFile)
+			throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean updateVirtualFile(VirtualFile virtualFile)
+			throws IOException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteVirtualFile(VirtualFile virtualFile)
+			throws IOException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
