ac9bc7f866e0145abdf97cf1c995394656c26a3a
Jason Wee <peichieh@gmail.com>
2015-11-22 17:56:43 +0800

    improved runme example and added cassandra 3.0 example

--- src/java/org/just4fun/voc/RunMe.java
+++ src/java/org/just4fun/voc/RunMe.java
@@ -3,13 +3,11 @@ package org.just4fun.voc;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import org.just4fun.voc.file.BinaryFile;
 import org.just4fun.voc.storage.Connection;
 import org.just4fun.voc.storage.cassandra.CassandraClientConfiguration;
+import org.just4fun.voc.storage.cassandra.ConnectionDatastaxDriver;
 import org.just4fun.voc.storage.cassandra.ConnectionHector;
 
 
@@ -22,23 +20,39 @@ public class RunMe {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-		// convert a binary file to hex.
-		//String hex = BinaryFile.toHex(new File("test.mp4"));
+		// convert a file to binary hex in string format.
+		String hex = BinaryFile.toHex(new File("test/resources/test.mp4"));
+		String part = hex.substring(0, 10);
+		System.out.println(part);
 		
-		// make binary file into cassandra file
-		//BinaryFile video = BinaryFile.getBinaryFile("", "fish.mp4", new File("test/resources/test.mp4"));
+		// make a binary file from a file.
+		BinaryFile video = BinaryFile.getBinaryFile("", "fish.mp4", new File("test/resources/test.mp4"));
 		
 		
 		// save binary file into cloud.
-		//CassandraClientConfiguration config = new CassandraClientConfiguration();
-		//Connection conn = new ConnectionHector(config);
-		//conn.storeBinary("fish.mp4", video);
-		//BinaryFile bf = conn.readBinary("fish.mp4");
-		//BinaryFile.toFile(bf.getHexContent(), "/home/jason/Desktop/fish.mp4");
-		//conn.deleteBinary("fish.mp4");
+		int useCassandraVersion = 1;
+		CassandraClientConfiguration config = null;
+		Connection conn = null;
+		BinaryFile bf = null;
 		
-		//Object[] o = new ArrayList()<String>[42];
-		
+		switch (useCassandraVersion) {
+		case 1:
+		// cassandra 1.2 or below
+		config = new CassandraClientConfiguration();
+		conn = new ConnectionHector(config);
+		conn.storeBinary("fish.mp4", video);
+		bf = conn.readBinary("fish.mp4");
+		BinaryFile.toFile(bf.getHexContent(), "/home/jason/Desktop/fish.mp4");
+		conn.deleteBinary("fish.mp4");
+		break;
+		case 3:
+		// cassandra 3.0
+		config = new CassandraClientConfiguration();
+		conn = new ConnectionDatastaxDriver(config);
+		bf = BinaryFile.getBinaryFile("/", "django.mp3", new File("./src/resources/django.mp3"));
+		conn.storeBinary("django.mp3", bf);
+		break;
+		}
 		
 		
 	}
