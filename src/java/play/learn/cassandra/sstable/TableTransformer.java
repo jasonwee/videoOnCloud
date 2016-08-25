cf03707ae441d8856918ebf98ef962e153aaaf11
Jason Wee <peichieh@gmail.com>
2016-08-25 17:32:16 +0800

    remove emtpy space

--- src/java/play/learn/cassandra/sstable/TableTransformer.java
+++ src/java/play/learn/cassandra/sstable/TableTransformer.java
@@ -3,7 +3,6 @@ package play.learn.cassandra.sstable;
 public class TableTransformer {
 
 	public static final String ANSI_RESET = "\u001B[0m";
-	
 	public static final String ANSI_BLUE = "\u001B[34m";
 	public static final String ANSI_CYAN = "\u001B[36m";
 	public static final String ANSI_WHITE = "\u001B[37m";
