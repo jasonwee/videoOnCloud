9ac056af4394ea115f9163ad8f6c08e727949b32
Jason Wee <peichieh@gmail.com>
2023-10-28 00:04:06 +0800

    sample

---
+++ udacity/introduction_to_tensorflow_lite/learn1.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+
+converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
+converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
+tflite_quant_model = converter.convert()
