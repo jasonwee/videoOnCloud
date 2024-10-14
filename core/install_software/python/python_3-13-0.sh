eb29f041b81dc44d0c933dd477e68b57c62c9c0c
Jason Wee <peichieh@gmail.com>
2024-10-14 22:36:19 +0800

    py3.13 install script

---
+++ core/install_software/python/python_3-13-0.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# run as root
+# docker run -it debian:latest bash
+# apt update
+# apt install -y wget xz-utils vim nano build-essential libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+
+# install
+cd /usr/local/src
+wget https://www.python.org/ftp/python/3.13.0/Python-3.13.0.tar.xz
+tar xf Python-3.13.0.tar.xz
+cd Python-3.13.0
+./configure --enable-optimizations --prefix=/opt/weetech/python-3.13.0 
+make
+# make test
+# if error, run clean below and redo on the configure
+# make clean && make distclean
+# if still error, just skip make and do make altinstall only
+make altinstall
+
+
+# check
+/opt/weetech/python-3.13.0/bin/pip3.13 --version
+/opt/weetech/python-3.13.0/bin/python3.13 --version
