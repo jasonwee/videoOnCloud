7f9ec20a5b3aed7c8813f62b4003d04e985496f0
Jason Wee <peichieh@gmail.com>
2026-02-14 13:42:35 +0800

    added golang version 1.26

---
+++ core/install_software/golang/golang_1-26.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#
+# sample output : golang_1-26.log
+# 
+GOLANG_VERSION=1.26.0
+
+mkdir -p /opt/weetech/
+cd /opt/weetech/
+wget https://go.dev/dl/go$GOLANG_VERSION.linux-amd64.tar.gz
+tar xvf go$GOLANG_VERSION.linux-amd64.tar.gz
+mv go go-$GOLANG_VERSION
+rm -f go$GOLANG_VERSION.linux-amd64.tar.gz
+cat > go-$GOLANG_VERSION/init-golang.sh <<EOF
+#!/bin/bash
+
+
+PATH=$PATH:/opt/weetech/go-$GOLANG_VERSION/bin
+export PATH
+EOF
